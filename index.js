d3a40efe4a29969d56f2b20da757ee011d574ee5
Laury Lutinclear <laury.lutin@gmail.com>
2025-03-28 17:27:29 +0100

    commit intit

---
+++ index.js
@@ -0,0 +1,5 @@
+const names =  process.env.MY_NAME;
+const city = process.env.MY_CITY;
+const language = process.env.MY_LANGUAGE; 
+
+console.log(`I am ${names}, wilder in ${city}, and I love ${language}`);
